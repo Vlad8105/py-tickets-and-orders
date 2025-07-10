d589d8538385bba3cd9c488b7ed4758ec4a49cfc
Vlad <7nomer7@gmail.com>
2025-07-10 20:49:27 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,4 +1,8 @@
+from typing import Any
+
 from django.db import models
+from django.db.models import UniqueConstraint
+from werkzeug.routing import ValidationError
 
 
 class Genre(models.Model):
@@ -17,7 +21,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -42,11 +46,66 @@ class CinemaHall(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(
-        to=CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
+        to=CinemaHall, on_delete=models.CASCADE,
+        related_name="movie_sessions"
     )
     movie = models.ForeignKey(
-        to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
+        to=Movie, on_delete=models.CASCADE,
+        related_name="movie_sessions"
     )
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(args, kwargs)
+        self.title = None
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    auto_now_add = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(to=Actor, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(
+        to=MovieSession, on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        to=Order, on_delete=models.CASCADE
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["movie_session", "order", "row", "seat"])
+        ]
+
+    def __str__(self) -> str:
+        return (
+            f"<Ticket: {self.movie_session.movie.title} "
+            f"{self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')} "
+            f"(row: {self.row}, seat: {self.seat})>"
+        )
+
+    def clean(self) -> None:
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError(
+                {"row": [
+                    f"row number must be in available range: "
+                    f"(1, rows): (1, {self.movie_session.cinema_hall.rows})"
+                ]}
+            )
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
---
+++ services/order.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db import transaction
+from datetime import datetime
+from db.models import Order, Ticket, MovieSession
+
+
+@transaction.atomic
+def create_order(tickets, username, date=None):
+    user = User.objects.get(username=username)
+    if date:
+        created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        order = Order.objects.create(user=user, created_at=created_at)
+    else:
+        order = Order.objects.create(user=user)
+
+    for ticket_data in tickets:
+        movie_session = MovieSession.objects.get(
+            id=ticket_data["movie_session"]
+        )
+        Ticket.objects.create(
+            order=order,
+            movie_session=movie_session,
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+        )
+    return order
+
+
+def get_orders(username=None):
+    if username:
+        user = User.objects.get(username=username)
+        return Order.objects.filter(user=user)
+    return Order.objects.all()
